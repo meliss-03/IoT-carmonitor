1f95eb57dc214d0ee99aaf22a46a490549e5002c
meliss-03 <77516283+meliss-03@users.noreply.github.com>
2024-05-22 23:37:48 -0600

    Update main.js

--- front-end/monitor/js/main.js
+++ front-end/monitor/js/main.js
@@ -1,37 +1,34 @@
 
 let respuesta = document.getElementById("respuesta");
 
-
 function callApiRequest() {
     axios
-    .get('http://3.137.139.50/iot-car-control/back-end/apis/getRegistro.php')
-  .then(function (response) {
-    // handle success
-    console.log(response);
+        .get('http://3.137.139.50/iot-car-control/back-end/apis/getRegistro.php')
+        .then(function (response) {
+            // handle success
+            console.log(response);
 
-    let respuestaServidor = response.data=='w'
-    ?"ADELANTE"
-    :response.data == 's'
-    ?"ATRAS"
-    :response.data == 'a'
-    ?"IZQUIERDA"
-    :response.data == 'd'
-    ?"DERECHA"
-    :"DETENER";
-     
+            let respuestaServidor = response.data == 'w'
+                ? "ADELANTE"
+                : response.data == 's'
+                ? "ATRÁS"
+                : response.data == 'a'
+                ? "IZQUIERDA"
+                : response.data == 'd'
+                ? "DERECHA"
+                : response.data == 'q'
+                ? "DETENER"
+                : "COMANDO NO RECONOCIDO";
 
-    respuesta.innerHTML="Respuesta: <strong>" + response.data + "</strong>";
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-
-    
+            respuesta.innerHTML = "Respuesta: <strong>" + respuestaServidor + "</strong>";
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            respuesta.innerHTML = "Error: <strong>No se pudo obtener la respuesta</strong>";
+        });
 }
 
+// Llamar a la función cada 2 segundos
 setInterval(callApiRequest, 2000);
 
