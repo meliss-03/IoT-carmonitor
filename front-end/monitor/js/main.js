508c2798cf383a56c9e1e73ac9b796a23d8be3d0
meliss-03 <77516283+meliss-03@users.noreply.github.com>
2024-05-24 14:30:39 -0600

    Update main.js

--- front-end/monitor/js/main.js
+++ front-end/monitor/js/main.js
@@ -3,7 +3,7 @@ let respuesta = document.getElementById("respuesta");
 
 function callApiRequest() {
     axios
-    .get('http://3.137.139.50/iot-car-control/back-end/apis/getRegistro.php')
+    .get('http://3.140.247.103/iot-car-control/back-end/apis/getRegistro.php')
   .then(function (response) {
     // handle success
     console.log(response);
